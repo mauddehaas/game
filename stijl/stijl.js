bb92aaac46c5fe60c1618211c6e8bd5f1fcd4933
maud <maud.dehaas@student.rocvf.nl>
2017-11-15 14:28:56 +0100

    opdracht 3 af

---
+++ stijl/stijl.js
@@ -0,0 +1,14 @@
+document.getElementById("bl1").style.backgroundColor = "Aqua";
+document.getElementById("bl2").style.backgroundColor = "Darkgreen";
+document.getElementById("bl3").style.backgroundColor = "Yellow";
+document.getElementById("bl4").style.backgroundColor = "Red";
+
+document.getElementById("bl1").style.marginTop = "0px";
+document.getElementById("bl2").style.marginTop = "20px";
+document.getElementById("bl3").style.marginTop = "50px";
+document.getElementById("bl4").style.marginTop = "0px";
+
+document.getElementById("bl1").style.padding = "20px";
+document.getElementById("bl2").style.paddingLeft = "20px";
+document.getElementById("bl3").style.paddingLeft = "40px";
+document.getElementById("bl4").style.paddingRight = "40px";
